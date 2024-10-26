a7f21d4c5fe247cbb4f31abba2ae3c8d54018e7c
Naroly24 <narolytolentin0024@gmail.com>
2024-10-26 18:19:03 -0400

    Update Program.cs

--- LibraryManagement/LibraryManagement.API/Program.cs
+++ LibraryManagement/LibraryManagement.API/Program.cs
@@ -3,13 +3,11 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configura el DbContext con la cadena de conexión
 builder.Services.AddDbContext<LibraryContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("LibraryDatabase")));
 
 var app = builder.Build();
 
-// Configuración adicional...
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
